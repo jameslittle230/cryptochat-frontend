959b73a59ea81445a46d334aac346abb0ce4fce6
James Little <littleguy23@gmail.com>
2018-04-14 15:11:43 +0200

    Generate AES envelope based on user input (no RSA yet)

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-var socket = io('http://localhost:8080/');
-
-
-socket.on('msg', function(msg){
-	app.messages.push(msg);
-});
-
 var app = new Vue({
 	el: '#app',
 	data: {
-		messageInput: "",
+		connectionWarning: false,
+
+		envelope: {
+			iv: "3bbdce68b2736ed96972d56865ad82a2",
+			ke: "a891f95cc50bd872e8fcd96cf5030535e273c5210570b3dcfa7946873d167c57",
+			km: "80070713463e7749b90c2dc24911e275",
+			payload: "asdfasdf",
+			seqnum: 587
+		},
+
 		messages: [
 			"This is a message",
 			"It has some text"
@@ -21,6 +23,51 @@ var app = new Vue({
 			socket.emit('msg', this.messageInput)
 			this.messageInput = "";
 			return;
+		},
+
+		generateEnvelope: function(e) {
+			e.preventDefault();
+
+			var iv = CryptoJS.enc.Hex.parse(this.envelope.iv);
+			var ke = CryptoJS.enc.Hex.parse(this.envelope.ke);
+			var km = CryptoJS.enc.Hex.parse(this.envelope.km);
+			var payload = this.envelope.payload;
+			var seqnum = this.envelope.seqnum;
+
+			var cipherobj = CryptoJS.AES.encrypt(
+				payload,
+				ke,
+				{
+					iv : iv, 
+				}
+			).ciphertext;
+
+			console.log(CryptoJS.enc.Hex.stringify(cipherobj));
+
+			var header = CryptoJS.enc.Hex.parse([
+				"0001",
+				"00",
+				("0000000" + cipherobj.sigBytes.toString(16)).substr(-8),
+				("0000000" + parseInt(seqnum).toString(16)).substr(-8)
+			].join(""));
+
+			var mac = CryptoJS.enc.Hex.stringify(CryptoJS.HmacSHA256(
+				header + iv + cipherobj,
+				km
+			));
+
+			var envelope = header + " " + iv + " " + cipherobj + " " + mac + " " + ke + " " + km + " " + "DEADBEEF";
+			console.log(envelope);
 		}
 	}
-})
+});
+
+if(io in window) {
+	app.connectionWarning = true;
+}
+
+var socket = io('http://localhost:8080/');
+
+socket.on('msg', function(msg){
+	app.messages.push(msg);
+});
