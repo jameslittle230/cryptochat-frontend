0721f7b9326264abfcdf21159fd784d0839422bc
James Little <littleguy23@gmail.com>
2018-04-20 15:29:14 +0200

    Use RSA keys from server to encrypt and sign envelope parts

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+var axios = require('axios');
+var CryptoJS = require('crypto-js');
+var NodeRSA = require('node-rsa');
+
 try { io } catch(e) {
 	app.connectionWarning = true;
 }
 
 var socket;
+
 if(window.location.hostname == "localhost" || window.location.hostname == "127.0.0.1") {
 	socket = io('http://localhost:8080/');
 	axios.defaults.baseURL = 'http://localhost:8080/';
@@ -24,68 +29,83 @@ var app = new Vue({
 	data: {
 		connectionWarning: false,
 
-		envelope: {
+		messageDraftData: {
 			iv: "3bbdce68b2736ed96972d56865ad82a2",
 			ke: "a891f95cc50bd872e8fcd96cf5030535e273c5210570b3dcfa7946873d167c57",
-			snd: "",
+			snd: "1",
 			rcv: "",
 			payload: "asdfasdf",
-			seqnum: 587
+			seqnum: 587,
+			rcvPubPem: "",
+			sndPrivPem: ""
 		},
 
 		messages: [],
 		users: [],
+
+		newuser_username: "",
+		newuser_password: "",
 	},
 
-	methods: {
-		sendMessage: function(e) {
-			e.preventDefault();
-
-			var iv = CryptoJS.enc.Hex.parse(this.envelope.iv);
-			var ke = CryptoJS.enc.Hex.parse(this.envelope.ke);
-			var payload = this.envelope.payload;
-			var seqnum = this.envelope.seqnum;
-			var snd = this.envelope.snd;
-			var rcv = this.envelope.rcv;
+	computed: {
+		envelope: function() {
+			var iv = CryptoJS.enc.Hex.parse(this.messageDraftData.iv);
+			var ke = CryptoJS.enc.Hex.parse(this.messageDraftData.ke);
+			var payload = this.messageDraftData.payload;
+			var seqnum = this.messageDraftData.seqnum;
+			var snd = this.messageDraftData.snd;
+			var rcv = this.messageDraftData.rcv;
 			var cht = 0;
+			var sig = "";
 
 			var cipherobj = CryptoJS.AES.encrypt(
-				payload,
-				ke,
-				{
-					iv : iv, 
-				}
+				payload, ke, {iv: iv}
 			).ciphertext;
-
-			console.log(CryptoJS.enc.Hex.stringify(cipherobj));
 
 			var header = CryptoJS.enc.Hex.parse([
 				"0001",
 				"00",
 				("00000000" + cipherobj.sigBytes.toString(16)).substr(-8),
 				("00000000" + parseInt(seqnum).toString(16)).substr(-8),
-				("0000" + parseInt(snd).toString(16)).substr(-8),
-				("0000" + parseInt(rcv).toString(16)).substr(-8),
-				("0000" + parseInt(cht).toString(16)).substr(-8),
-				("000000000000" + Date.now().toString(16)).substr(-8)
+				("0000" + parseInt(snd).toString(16)).substr(-4),
+				("0000" + parseInt(rcv).toString(16)).substr(-4),
+				("0000" + parseInt(cht).toString(16)).substr(-4),
+				("000000000000" + Date.now().toString(16)).substr(-12)
 			].join(""));
 
-			var envelope = header + iv + cipherobj + ke + "deadbeef";
-			console.log(envelope);
-			socket.emit('msg', envelope);
+			if(this.messageDraftData.rcvPubPem && this.messageDraftData.sndPrivPem) {
+				var rcvPubkey = new NodeRSA();
+				rcvPubkey.importKey(this.messageDraftData.rcvPubPem);
+				ke = rcvPubkey.encrypt(ke, 'hex', 'hex');
+
+				var sndPrivkey = new NodeRSA(this.messageDraftData.sndPrivPem, 'private');
+				var sigData = header + iv + cipherobj + ke;
+				sig = sndPrivkey.sign(sigData, 'hex', 'hex');
+			}
+
+			var envelope = header + iv + cipherobj + ke + sig;
+			return envelope;
+		}
+	},
+
+	methods: {
+		sendMessage: function(e) {
+			socket.emit('msg', this.envelope);
+			this.randomizeThings();
+		},
+
+		randomizeThings: function() {
+			this.envelope.seqnum++;
+
 		},
 
 		getMessagesFromDatabase: function() {
 			axios.get('messages?recipient=2')
 			.then(function (response) {
-				console.log(response.data);
 				response.data.forEach(function(r) {
 					app.messages.push(r.content)
 				})
 			})
-			.catch(function (error) {
-				console.log(error);
-			});
 		},
 
 		getUsersFromDatabase: function() {
@@ -93,11 +113,36 @@ var app = new Vue({
 			.then(function(response) {
 				app.users = response.data
 			});
+		},
+
+		createNewUser: function() {
+			alert("New user");
+		},
+
+		getPublicKey: function(user_id) {
+			axios.get('publicKey?user_id=' + user_id)
+			.then(function(response) {
+				app.messageDraftData.rcvPubPem = response.data;
+			});
+		},
+
+		getPrivateKey: function(user_id) {
+			axios.get('privateKey?user_id=' + user_id)
+			.then(function(response) {
+				app.messageDraftData.sndPrivPem = response.data;
+			});
+		}
+	},
+
+	watch: {
+		'messageDraftData.rcv': function(val) {
+			this.getPublicKey(val);
 		}
 	},
 
 	mounted: function () {
 		this.getMessagesFromDatabase();
 		this.getUsersFromDatabase();
+		this.getPrivateKey(this.messageDraftData.snd);
 	}
 });
